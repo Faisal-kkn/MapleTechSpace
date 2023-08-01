bbaf8216650f1979fdfc7adef92eb6fc2ba633a6
Faisal K <faisal@penieltech.com>
2023-08-02 00:03:53 +0530

    feat(validation): form validation and task 1 done

--- task1/src/App.js
+++ task1/src/App.js
@@ -1,8 +1,10 @@
+import React, { useState } from 'react';
 import { HiOutlineUser } from 'react-icons/hi';
 import { FiMail } from 'react-icons/fi';
 import { BiLock, BiLogoLinkedin } from 'react-icons/bi';
 import { FaFacebookF } from 'react-icons/fa';
 import { AiOutlineGooglePlus } from 'react-icons/ai';
+import { useForm } from 'react-hook-form';
 
 const socialMedia = [
   FaFacebookF,
@@ -11,6 +13,23 @@ const socialMedia = [
 ]
 
 function App() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm({ name: '', email: '', password: '' });
+
+  const [formData, setFormData] = useState({})
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const handleFormSubmit = () => {
+    console.log(formData);
+  }
+
   return (
     <main className='flex justify-center items-center w-full min-h-screen px-4'>
       <div className="bg-white rounded-2xl shadow-2xl lg:flex overflow-hidden md:w-4/5 lg:w-3/5">
@@ -32,23 +51,23 @@ function App() {
             </div>
             <p className='text-gray-400 mb-4 text-sm'>or use your email for registration:</p>
             <div className='flex flex-col items-center'>
-              <form className='w-full'>
+              <form className='w-full' onSubmit={handleSubmit(handleFormSubmit)}>
                 <div className='mx-auto w-[75%]'>
                   <div className='bg-gray-100 p-2 flex items-center'>
-                    <HiOutlineUser className='text-gray-400 m-2 h-6 w-6' />
-                    <input type="text" name="name" id="name" placeholder='Name' className='bg-gray-100 outline-none text-sm font-medium flex-1' />
+                    <HiOutlineUser className='text-gray-400 m-2 h-5 w-5' />
+                    <input type="text" value={formData?.name} {...register('name', { required: true, minLength: 4, maxLength: 20 })} onChange={handleChange} id="name" placeholder='Name' className='bg-gray-100 outline-none text-sm font-medium flex-1' />
                   </div>
-                  <p className='font-normal text-xs m-0  mb-3 text-left text-red-600'>Error</p>
+                  <p className='font-normal text-xs m-0  mb-3 text-left text-red-600'> {errors.name && 'Name is required and min 4 max 20'}</p>
                   <div className='bg-gray-100 p-2 flex items-center'>
-                    <FiMail className='text-gray-400 m-2 h-6 w-6' />
-                    <input type="email" name="email" id="email" placeholder='Email' className='bg-gray-100 outline-none text-sm font-medium flex-1' />
+                    <FiMail className='text-gray-400 m-2 h-5 w-5' />
+                    <input type="email" value={formData?.email} {...register("email", { required: true, pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ })} onChange={handleChange} id="email" placeholder='Email' className='bg-gray-100 outline-none text-sm font-medium flex-1' />
                   </div>
-                  <p className='font-normal text-xs m-0  mb-3 text-left text-red-600'>Error</p>
+                  <p className='font-normal text-xs m-0  mb-3 text-left text-red-600'> {errors.email && 'Email is required'}</p>
                   <div className='bg-gray-100 p-2 flex items-center'>
-                    <BiLock className='text-gray-400 m-2 h-6 w-6' />
-                    <input type="password" name="password" id="password" placeholder='Password' className='bg-gray-100 outline-none text-sm font-medium flex-1' />
+                    <BiLock className='text-gray-400 m-2 h-5 w-5' />
+                    <input type="password" value={formData?.password} {...register("password", { required: true, pattern: /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[^\s]+$/ })} onChange={handleChange} id="password" placeholder='Password' className='bg-gray-100 outline-none text-sm font-medium flex-1' />
                   </div>
-                  <p className='font-normal text-xs m-0 mb-5 text-left text-red-600'>Error</p>
+                  <p className='font-normal text-xs m-0  mb-3 text-left text-red-600'> {errors.password && 'Password is required (Should be at least one Capital letter, symbol and number)'}</p>
                   <Button background={true} content={'Sign up'} type={true} />
                 </div>
               </form>
